e508ffad8b64461c9628248a2144a81ea9b76f14
Alejandro Gaviria <mr.gaviria@gmail.com>
2018-03-02 16:56:08 -0500

    Added token lexeme types.  These model the lox language for the most part but are subject to change.

---
+++ src/token.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    // Single character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    // Literals.
+    Identifier(String),
+    StrLiteral(String),
+    NumLiteral(f64),
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Func,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    // Comment and whitespace.
+    Comment,
+    Whitespace,
+}
