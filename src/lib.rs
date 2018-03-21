c3bf4ca4561b290978f2a46e00e6f988cd67646e
Alejandro Gaviria <mr.gaviria@gmail.com>
2018-03-21 01:02:27 -0400

    Pruned lib to provide integration testing. Included comment integration test.

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+extern crate itertools;
+#[cfg_attr(test, macro_use)]
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+extern crate rug;
+
+pub mod errors;
+pub mod scanner;
+pub mod utils;
+pub mod parser;
---
+++ tests/integration.rs
@@ -0,0 +1,29 @@
+extern crate argentum;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+use argentum::parser;
+
+macro_rules! integration_test {
+  ($id:ident, $name:expr, $is_ok:expr) => {
+    #[test]
+    fn $id() {
+      let mut fixture = File::open(format!("tests/fixtures/{}.ag", $name))
+        .expect(&format!("failed to open tests/fixtures/{}.ag", $name));
+      let mut buf = String::new();
+      fixture.read_to_string(&mut buf)
+        .expect(&format!("failed to read tests/fixtures/{}.ag", $name));
+      let outcome = parser::parse(&buf);
+      assert_eq!(
+        outcome.is_ok(),
+        $is_ok,
+        "failed to parse tests/fixtures/{}.ag: {:?}",
+        $name,
+        outcome
+        );
+    }
+  }
+}
+
+integration_test!(test_comment, "comment", true);
